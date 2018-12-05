2b6e4bcb8688290dca45d449b7e81735882f29b2
Michael Zöhling <Michael.Zoehling@outlook.com>
2018-12-05 14:36:20 +0100

    DataBinding

---
+++ DatabindingDemo/DatabindingDemo/Farbkonverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace DatabindingDemo
+{
+    class Farbkonverter : IValueConverter
+    {
+        // Nur OneWayBinding
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string color = (string)value;
+
+            if (color == null)
+                return Color.Black;
+
+            var fields = typeof(Color).GetFields();
+            var outputColor = fields.FirstOrDefault(x => x.Name.ToString().ToLower() == color.ToLower());
+            return outputColor?.GetValue(typeof(Color)) ?? Color.Black;
+        }
+
+        // Pflicht bei TwoWayBinding und OneWayToSource, ansonsten "weglassen"
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DatabindingDemo/DatabindingDemo/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DatabindingDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        //private void SliderWert_ValueChanged(object sender, ValueChangedEventArgs e)
+        //{
+        //    labelWert.Text = sliderWert.Value.ToString();
+        //}
+    }
+}
