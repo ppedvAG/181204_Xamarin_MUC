7d53684e3aa1f02c68c2f46a09c0ea7c3da4e10b
Michael Zöhling <Michael.Zoehling@outlook.com>
2018-12-07 14:17:32 +0100

    Xamarin.Essential Connectivity-Test

---
+++ XamarinEssentialsDemo/XamarinEssentialsDemo/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace XamarinEssentialsDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ContentPage_Appearing(object sender, EventArgs e)
+        {
+            labelConnectivity.Text = Connectivity.NetworkAccess.ToString();
+        }
+    }
+}
