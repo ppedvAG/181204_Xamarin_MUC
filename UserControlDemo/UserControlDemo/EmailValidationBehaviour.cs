b763d9e9850af620b2c569aaf553e58e71d35043
Michael Zöhling <Michael.Zoehling@outlook.com>
2018-12-07 09:59:18 +0100

    LabledEntry und Behavior

---
+++ UserControlDemo/UserControlDemo/EmailValidationBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Text;
+using Xamarin.Forms;
+
+namespace UserControlDemo
+{
+    class EmailValidationBehavior : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            bindable.TextChanged += EmailValidation;
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            bindable.TextChanged -= EmailValidation;
+            base.OnDetachingFrom(bindable);
+        }
+
+        private void EmailValidation(object sender, TextChangedEventArgs e)
+        {
+            Entry input = sender as Entry;
+            input.TextColor = Color.Default;
+            try
+            {
+                MailAddress mail = new MailAddress(input.Text);
+            }
+            catch (FormatException)
+            {
+                input.TextColor = Color.Red;
+            }
+        }
+    }
+}
---
+++ UserControlDemo/UserControlDemo/LabledEntry.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace UserControlDemo
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LabledEntry : ContentView
+	{
+		public LabledEntry ()
+		{
+			InitializeComponent ();
+            this.BindingContext = this;
+		}
+
+
+        // propdp -> DependencyProperty aus WPF - Snippet
+        // 1) propdp snippet erstellen
+        // 2) Auf Xamarin.Forms "Umbauen"
+
+        // Änderungen vom propdp - snippet:
+        // DependencyProperty durch BindableProperty ersetzen
+        // propertymetadata durch "standardwert" ersetzen (oder auslassen)
+        // statt .Register .Create nutzen
+
+        public string Label
+        {
+            get { return (string)GetValue(LabelProperty); }
+            set { SetValue(LabelProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Label.  This enables animation, styling, binding, etc...
+        public static readonly BindableProperty LabelProperty =
+            BindableProperty.Create("Label", typeof(string), typeof(LabledEntry),string.Empty);
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Label.  This enables animation, styling, binding, etc...
+        public static readonly BindableProperty TextProperty =
+            BindableProperty.Create("Text", typeof(string), typeof(LabledEntry), string.Empty);
+
+        public string Placeholder
+        {
+            get { return (string)GetValue(PlaceholderProperty); }
+            set { SetValue(PlaceholderProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Label.  This enables animation, styling, binding, etc...
+        public static readonly BindableProperty PlaceholderProperty =
+            BindableProperty.Create("Placeholder", typeof(string), typeof(LabledEntry), string.Empty);
+
+    }
+}
